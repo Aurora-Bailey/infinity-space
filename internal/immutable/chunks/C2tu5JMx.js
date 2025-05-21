dae4223f718c0c2dade16fc51e12135655a15050
Aurora Bailey <aurora.i.bailey@gmail.com>
2025-05-21 12:19:49 -0600

    Updates

---
+++ internal/immutable/chunks/C2tu5JMx.js
@@ -0,0 +1,1 @@
+import{w as s}from"./CfPaVNeV.js";const n=s(""),o=s(0);export{o as a,n as s};
--- internal/immutable/nodes/3.CuaBeyM7.js
+++ internal/immutable/nodes/3.CuaBeyM7.js
@@ -1,4 +1,4 @@
-import{t,h as p,a as e}from"../chunks/J3dmLmji.js";import"../chunks/_PeJ6quD.js";import{$ as r}from"../chunks/C7EWuEMJ.js";var l=t('<meta name="description" content="About this app">'),s=t(`<div class="text-column"><h1>About this app</h1> <p>This is a <a href="https://svelte.dev/docs/kit">SvelteKit</a> app. You can make your own by typing
+import{t,h as p,a as e}from"../chunks/Dbj2R7oW.js";import"../chunks/Bx-0Knem.js";import{$ as r}from"../chunks/DhS4ZT9p.js";var l=t('<meta name="description" content="About this app">'),s=t(`<div class="text-column"><h1>About this app</h1> <p>This is a <a href="https://svelte.dev/docs/kit">SvelteKit</a> app. You can make your own by typing
 		the following into your command line and following the prompts:</p> <pre>npx sv create</pre> <p>The page you're looking at is purely static HTML, with no client-side interactivity needed.
 		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
 		the devtools network panel and reloading.</p> <p>The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
